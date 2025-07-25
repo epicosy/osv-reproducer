b33d02e28bb372a1a70c482fd9d7a7e219b892b3
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2025-07-25 12:47:56 +0100

    fixed import;

--- osv_reproducer/core/models/__init__.py
+++ osv_reproducer/core/models/__init__.py
@@ -2,4 +2,4 @@ from .paths import PathsLayout
 from .project import ProjectInfo
 from .report import OSSFuzzIssueReport
 from .context import ReproductionContext
-from .result import CrashInfo, ReproductionResult
+from .result import CrashInfo
